a295e0e696b7a6880b77061767f648527230405c
cettipao <cettipao@live.com.ar>
2019-10-06 15:55:06 -0300

    First Commit

---
+++ src/sistemacheckin/Pasajero.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemacheckin;
+
+/**
+ *-nombre: String
+-apellido: String
+-dni: String
+-Vuelo: vuelo
+
+ * @author cetti
+ */
+public class Pasajero {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private Vuelo vuelo;
+    
+}
---
+++ src/sistemacheckin/SistemaCheckIn.java
@@ -0,0 +1,17 @@
+/*
+ * Trabajo Practico Integrador
+ * Profesores: Nicolas Fanin, Martín Navarro y Jeremías Antequera. 
+ * Cetti Paolo
+ */
+package sistemacheckin;
+
+public class SistemaCheckIn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
